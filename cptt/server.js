bf5becd7eb74efe748754d1f92f2a69c32cb424a
Ensar Kayacan <kycnensar@gmail.com>
2026-09-10 15:55:19 +0300

    web site guncelleme 10.09.2026

--- cptt/server.js
+++ cptt/server.js
@@ -6,12 +6,19 @@ const PORT = 4000;
 
 app.use(express.static(path.join(__dirname)));
 
-app.get('/anasayfa', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.get('/darulmu-AI-limin', (req, res) => res.sendFile(path.join(__dirname, 'pages/ai/darulmu-AI-limin.html')));
-app.get('/anka', (req, res) => res.sendFile(path.join(__dirname, 'pages/iha/anka.html')));
-app.get('/rok-et', (req, res) => res.sendFile(path.join(__dirname, 'pages/roket/rok-et.html')));
-app.get('/abra', (req, res) => res.sendFile(path.join(__dirname, 'pages/oyun/abra.html')));
-app.get('/frc', (req, res) => res.sendFile(path.join(__dirname, 'pages/frc/frc.html')));
+const routes = {
+  '/anasayfa': 'index.html',
+  '/darulmu-AI-limin': 'pages/ai/darulmu-AI-limin.html',
+  '/anka': 'pages/iha/anka.html',
+  '/rok-et': 'pages/roket/rok-et.html',
+  '/abra': 'pages/oyun/abra.html',
+  '/frc': 'pages/frc/frc.html'
+};
+
+Object.entries(routes).forEach(([route, file]) => {
+  app.get(route, (req, res) => res.sendFile(path.join(__dirname, file)));
+  app.get(route + '/', (req, res) => res.sendFile(path.join(__dirname, file)));
+});
 
 app.listen(PORT, () => {
   console.log(`Sunucu çalışıyor: http://localhost:${PORT}`);
